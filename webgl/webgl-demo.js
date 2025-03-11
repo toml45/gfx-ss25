23a1ca57568a5836f0ea3d00e937ec759d51e0b3
tomabungis45 <kollodoku123@gmail.com>
2025-03-11 15:24:39 +0100

    typescript setup

--- webgl/webgl-demo.js
+++ webgl/webgl-demo.js
@@ -1,16 +1,16 @@
+"use strict";
 main();
-
 function main() {
     const canvas = document.querySelector("#gl-canvas");
-    const gl = canvas.getContext("webgl");
-
-    if (gl === null) {
-        alert(
-            "Unable to initialize WebGL. Your browser or machine may not support it.",
-        );
+    if (canvas === null) {
+        alert("No canvas element was found");
         return;
     }
-
+    const gl = canvas.getContext("webgl");
+    if (gl === null) {
+        alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+        return;
+    }
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 }
---
+++ webgl/webgl-demo.ts
@@ -0,0 +1,20 @@
+main();
+
+function main() {
+    const canvas: HTMLCanvasElement | null = document.querySelector("#gl-canvas");
+    if (canvas === null) {
+        alert("No canvas element was found")
+        return;
+    }
+
+    const gl: WebGLRenderingContext | null = canvas.getContext("webgl");
+    if (gl === null) {
+        alert(
+            "Unable to initialize WebGL. Your browser or machine may not support it.",
+        );
+        return;
+    }
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
